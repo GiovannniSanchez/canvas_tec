bb3a8587de0f3cb71933fdad461d170138c2705e
Giovanni Sanchez <l20590311@sjuanrio.tecnm.mx>
2023-04-13 16:18:48 -0600

    Se genera consultas relacionadas al usuario: Estas consultas muestran solo la informacion de las respuestas del usuario basadas en el formulario test01.html

--- backds/models.py
+++ backds/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class answers_user(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=False)
     answer1=models.TextField(max_length=1000, unique=False, verbose_name='Respuesta1')
     answer2=models.TextField(max_length=1000, unique=False, verbose_name='Respuesta2')
     answer3=models.TextField(max_length=1000, unique=False, verbose_name='Respuesta3')
--- backds/views.py
+++ backds/views.py
@@ -5,61 +5,71 @@ from .forms import RegisterAnswer
 from django.http import HttpResponse
 import openai
 
+
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
+
+
 @login_required
 def prueba(request):
-    #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
+    # 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
     openai.api_key = "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"
 
     # Contexto del asistente
-    variable='anime'
-    messages = [{"role": "system", "content": "Eres un experto en modelos de negocio"+ 'y'+ variable}]
+    variable = 'anime'
+    messages = [{"role": "system", "content": "Eres un experto en modelos de negocio" + 'y' + variable}]
 
-    parte1='dame la lista de pasos'
-    parte2='para crear un modelo de:'
-    parte3='negocio'
-    contenido =parte1+parte2+parte3
-
+    parte1 = 'dame la lista de pasos'
+    parte2 = 'para crear un modelo de:'
+    parte3 = 'negocio'
+    contenido = parte1 + parte2 + parte3
 
     messages.append({"role": "user", "content": contenido})
     respuesta = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, max_tokens=1000)
     contenido_respuesta = respuesta.choices[0].message.content
     messages.append({"role": "assistant", "content": contenido_respuesta})
-    return render(request,'chatgpt.html',{
-        'contenido_respuesta':contenido_respuesta,
-        'contenido':contenido
+    return render(request, 'chatgpt.html', {
+        'contenido_respuesta': contenido_respuesta,
+        'contenido': contenido
     })
+
+
 @login_required
 def formulario(request):
-    return render(request,'formulario.html')
+    return render(request, 'formulario.html')
 
+
+@login_required
 def test01(request):
-        answers= answers_user.objects.all()
-        if request.method =='GET':
-            answers = answers_user.objects.all()
-            return render(request,'test01.html',{'answers':answers,
-                                                 'form':RegisterAnswer()})
-        else:
-            answers_user.objects.create(answer1=request.POST['answer1'],
-                                        answer2=request.POST['answer2'],
-                                        answer3=request.POST['answer3'],
-                                        answer4=request.POST['answer4'],
-                                        answer5=request.POST['answer5'],
-                                        answer6=request.POST['answer6'],
-                                        answer7=request.POST['answer7'],
-                                        answer8=request.POST['answer8'],
-                                        answer9=request.POST['answer9'],
-                                        answer10=request.POST['answer10'])
-            return redirect('About')
+    if request.method == 'GET':
+        user = request.user
+        answers = answers_user.objects.filter(user=user)
+
+        return render(request, 'test01.html', {'answers': answers,
+                                               'form': RegisterAnswer()})
+    else:
+
+        user_id = request.user.id
+        answers_user.objects.create(answer1=request.POST['answer1'],
+                                    answer2=request.POST['answer2'],
+                                    answer3=request.POST['answer3'],
+                                    answer4=request.POST['answer4'],
+                                    answer5=request.POST['answer5'],
+                                    answer6=request.POST['answer6'],
+                                    answer7=request.POST['answer7'],
+                                    answer8=request.POST['answer8'],
+                                    answer9=request.POST['answer9'],
+                                    answer10=request.POST['answer10'], user_id=user_id)
+        return redirect('About')
+
+
 @login_required
 def canvas(request):
-    Problema='este es'
-    variable2='un texto de prueba'
-    variable3='prueba respuesta xddd'
-    return  render(request, 'canvas.html',{
+    Problema = 'este es'
+    variable2 = 'un texto de prueba'
+    variable3 = 'prueba respuesta xddd'
+    return render(request, 'canvas.html', {
         'texto1': Problema,
         'texto2': variable2,
         'texto3': variable3
     })
-
